f51ef3d3fd660003ca342156f7aba32864d8251d
Александр Гладун <alexandr20021995@gmail.com>
2024-01-31 12:23:22 +0600

    refactor api

--- src/main/java/com/goh/teledone/api/controller/TaskController.java
+++ src/main/java/com/goh/teledone/api/controller/TaskController.java
@@ -2,12 +2,9 @@ package com.goh.teledone.api.controller;
 
 import com.goh.teledone.api.dto.TaskDto;
 import com.goh.teledone.api.service.TaskService;
-import com.goh.teledone.taskmanager.TaskListType;
-import com.goh.teledone.taskmanager.TaskManagerServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,60 +12,22 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService taskService;
-    private final TaskManagerServiceImpl managerService;
 
-    public TaskController(TaskService taskService, TaskManagerServiceImpl managerService) {
+    public TaskController(TaskService taskService) {
         this.taskService = taskService;
-        this.managerService = managerService;
     }
-/*
 
-    @GetMapping(value = "/tasks/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<TaskDto> getTasks(@PathVariable(value = "id") String id) {
-        log.info("Get tasks request");
-        log.info("User telegram id = " + id);
-        Long userId = Long.valueOf(id);
-        List<Task> inboxTasks = managerService.getTasks(userId, TaskListType.INBOX);
-        List<Task> todayTasks = managerService.getTasks(userId, TaskListType.TODAY);
-        List<Task> weekTasks = managerService.getTasks(userId, TaskListType.WEEK);
-        List<Task> backlogTasks = managerService.getTasks(userId, TaskListType.BACKLOG);
-
-        List<TaskDto> responseList = new ArrayList<>();
-
-        inboxTasks.forEach((task) ->
-                responseList.add(new TaskDto(
-                        task.getTitle(),
-                        TaskListType.INBOX.name()
-                ))
-        );
-        todayTasks.forEach((task) ->
-                responseList.add(new TaskDto(
-                        task.getTitle(),
-                        TaskListType.TODAY.name()
-                ))
-        );
-        weekTasks.forEach((task) ->
-                responseList.add(new TaskDto(
-                        task.getTitle(),
-                        TaskListType.WEEK.name()
-                ))
-        );
-        backlogTasks.forEach((task) ->
-                responseList.add(new TaskDto(
-                        task.getTitle(),
-                        TaskListType.BACKLOG.name()
-                ))
-        );
-        log.info("Get tasks request. tasks: " + inboxTasks);
-        return responseList;
+    @GetMapping(value = "/tasks/{userId}")
+    public List<TaskDto> getTasks(@PathVariable(value = "userId") String userId) {
+        return taskService.getAllTasksFromVault(Long.parseLong(userId));
     }
-*/
+
 
     @PostMapping(value = "/tasks/{userId}")
-    public List<TaskDto> addAndGetTasks(@RequestBody List<TaskDto> taskDtoList,
+    public List<TaskDto> addTasks(@RequestBody List<TaskDto> taskDtoList,
                                         @PathVariable(value = "userId") String userId) {
         Long telegramUserId = Long.valueOf(userId);
-        return taskService.addAndGetTasks(telegramUserId, taskDtoList);
+        return taskService.addTasks(telegramUserId, taskDtoList);
     }
 
     @PutMapping(value = "/tasks/{userId}")
--- src/main/java/com/goh/teledone/api/service/TaskService.java
+++ src/main/java/com/goh/teledone/api/service/TaskService.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -24,12 +23,31 @@ public class TaskService {
         this.managerService = managerService;
     }
 
-    public List<TaskDto> addAndGetTasks(Long userId, List<TaskDto> taskDtoList) {
-        log.info("Synchronize tasks request");
+    public List<TaskDto> getAllTasksFromVault(Long userId) {
+        List<Task> inboxTasks = managerService.getTasks(userId, TaskListType.INBOX);
+        List<Task> todayTasks = managerService.getTasks(userId, TaskListType.TODAY);
+        List<Task> weekTasks = managerService.getTasks(userId, TaskListType.WEEK);
+        List<Task> backlogTasks = managerService.getTasks(userId, TaskListType.BACKLOG);
+        List<Task> doneTasks = managerService.getTasks(userId, TaskListType.DONE);
+
+        List<TaskDto> responseList = new ArrayList<>();
+
+        mergeTaskLists(responseList, inboxTasks, TaskListType.INBOX);
+        mergeTaskLists(responseList, todayTasks, TaskListType.TODAY);
+        mergeTaskLists(responseList, weekTasks, TaskListType.WEEK);
+        mergeTaskLists(responseList, backlogTasks, TaskListType.BACKLOG);
+        mergeTaskLists(responseList, doneTasks, TaskListType.DONE);
+
+        log.info("getAllTasksFromVault(), responseList = " + responseList);
+
+        return responseList;
+    }
+
+    public List<TaskDto> addTasks(Long userId, List<TaskDto> taskDtoList) {
+        log.info("Add tasks request");
         log.info("User telegram id = " + userId);
 
         List<TaskDto> responseList = new ArrayList<>(taskDtoList);
-
         for (int i = 0; i < taskDtoList.size(); i++) {
             TaskDto task = taskDtoList.get(i);
             if (task.getId() == 0) {
@@ -40,45 +58,16 @@ public class TaskService {
             responseList.set(i, task);
         }
 
-        // check tasks which absent in client but there are in server
-        List<TaskDto> tasksFromDb = getAllTasksFromVault(userId);
-        for (TaskDto dbTask : tasksFromDb) {
-            boolean isFound = false;
-            for (TaskDto responseTask : responseList) {
-                if (dbTask.getId().equals(responseTask.getId())) {
-                    isFound = true;
-                    break;
-                }
-            }
-            if (!isFound) {
-                dbTask.setLocalId(UUID.randomUUID().toString());
-                responseList.add(dbTask);
-            }
-        }
+        checkConsistentData(userId, responseList);
 
-        // check delete task by server
-        List<TaskDto> tempResponseTask = new ArrayList<>(responseList);
-        for (TaskDto responseTask : tempResponseTask) {
-            log.info("checking responseTask = " + responseTask);
-            boolean isFoundDeleted = true;
-            for (TaskDto dbTask : tasksFromDb) {
-                if (dbTask.getId().equals(responseTask.getId())) {
-                    isFoundDeleted = false;
-                    break;
-                }
-            }
-            if (isFoundDeleted) {
-                responseList.remove(responseTask);
-                log.info("Deleted redundant responseTask = " + responseTask);
-            }
-        }
-
-        log.info("addAndGetTasks(), responseList = " + responseList);
+        log.info("addTasks(), responseList = " + responseList);
 
         return responseList;
     }
 
     public List<TaskDto> editAndMoveTasks(Long userId, List<TaskDto> taskDtoList) {
+        log.info("Edit tasks request");
+        log.info("User telegram id = " + userId);
         List<TaskDto> responseList = new ArrayList<>(taskDtoList);
         List<TaskDto> tasksFromDb = getAllTasksFromVault(userId);
         for (TaskDto task : taskDtoList) {
@@ -105,11 +94,11 @@ public class TaskService {
 
     public void deleteTask(Long userId, Long taskId) {
         log.info("Deleting task with id = " + taskId);
+        log.info("User telegram id = " + userId);
         List<TaskDto> tasksFromDb = getAllTasksFromVault(userId);
         for (TaskDto dbTask : tasksFromDb) {
             if (Objects.equals(taskId, dbTask.getId())) {
                 managerService.delete(userId, taskId);
-                log.info("Task deleted with id = " + taskId);
                 break;
             }
         }
@@ -126,36 +115,6 @@ public class TaskService {
         managerService.moveToTaskList(userId, task.getId(), type, Instant.parse(task.getModifyDate()));
     }
 
-    private List<TaskDto> getAllTasksFromVault(Long userId) {
-        List<Task> inboxTasks = managerService.getTasks(userId, TaskListType.INBOX);
-        List<Task> todayTasks = managerService.getTasks(userId, TaskListType.TODAY);
-        List<Task> weekTasks = managerService.getTasks(userId, TaskListType.WEEK);
-        List<Task> backlogTasks = managerService.getTasks(userId, TaskListType.BACKLOG);
-        List<Task> doneTasks = managerService.getTasks(userId, TaskListType.DONE);
-
-        List<TaskDto> responseList = new ArrayList<>();
-
-        mergeTaskLists(responseList, inboxTasks, TaskListType.INBOX);
-        mergeTaskLists(responseList, todayTasks, TaskListType.TODAY);
-        mergeTaskLists(responseList, weekTasks, TaskListType.WEEK);
-        mergeTaskLists(responseList, backlogTasks, TaskListType.BACKLOG);
-        mergeTaskLists(responseList, doneTasks, TaskListType.DONE);
-
-        /*for (Task doneTask : doneTasks) {
-            for (int j = 0; j < responseList.size(); j++) {
-                TaskDto responseTask = responseList.get(j);
-                if (Objects.equals(doneTask.getId(), responseTask.getId())) {
-                    responseTask.setDone(true);
-                    responseList.set(j, responseTask);
-                }
-            }
-        }*/
-
-        log.info("getAllTasksFromVault(), responseList = " + responseList);
-
-        return responseList;
-    }
-
     private void mergeTaskLists(List<TaskDto> responseList, List<Task> inputList, TaskListType type) {
         inputList.forEach((task) ->
                 responseList.add(new TaskDto(
@@ -169,6 +128,42 @@ public class TaskService {
                         task.isDone()
                 ))
         );
+    }
+
+    private void checkConsistentData(Long userId, List<TaskDto> checkingList) {
+
+        // check added tasks by telegram
+        List<TaskDto> tasksFromDb = getAllTasksFromVault(userId);
+        for (TaskDto dbTask : tasksFromDb) {
+            boolean isFound = false;
+            for (TaskDto responseTask : checkingList) {
+                if (dbTask.getId().equals(responseTask.getId())) {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound) {
+                dbTask.setLocalId(UUID.randomUUID().toString());
+                checkingList.add(dbTask);
+            }
+        }
+
+        // check deleted tasks by telegram
+        List<TaskDto> tempResponseTask = new ArrayList<>(checkingList);
+        for (TaskDto responseTask : tempResponseTask) {
+            log.info("checking responseTask = " + responseTask);
+            boolean isFoundDeleted = true;
+            for (TaskDto dbTask : tasksFromDb) {
+                if (dbTask.getId().equals(responseTask.getId())) {
+                    isFoundDeleted = false;
+                    break;
+                }
+            }
+            if (isFoundDeleted) {
+                checkingList.remove(responseTask);
+                log.info("Deleted redundant responseTask = " + responseTask);
+            }
+        }
     }
 
 }
